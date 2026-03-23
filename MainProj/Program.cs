4e4a87202aa88959f27174aa577d885f35e27368
MrdDarr12 <mrddar@yahoo.com>
2026-03-23 21:23:12 +0400

    Add files via upload

---
+++ MainProj/Program.cs
@@ -0,0 +1,27 @@
+﻿using MyLinkedListProgramm;
+
+namespace MainProj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+            MyQueue<int> queue = new MyQueue<int>();   
+            stack.Push(1);
+            stack.Push(2);  
+            stack.Push(3);
+
+            Console.WriteLine(stack.Peek());
+            Console.WriteLine(stack.Pop());
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);   
+            queue.Enqueue(3);
+
+            Console.WriteLine(queue.Peek());
+            Console.WriteLine(queue.Dequeue());
+
+        }
+    }
+}
